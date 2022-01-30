6d42270e53da4e799469d982adbbba0cd405e909
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2022-01-30 14:53:22 +0000

    changed the cluster name

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -13,7 +13,7 @@ terraform {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
-  cluster_name    = "cluster-Test-GZ"
+  cluster_name    = "infra-cluster-Test"
   cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
 
@@ -29,7 +29,7 @@ module "eks" {
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-2"
